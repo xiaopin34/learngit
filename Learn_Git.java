efed85ab3f8043743e8587dd8db23eb1ab1cf7b0
xiaopin34 <caoxiaopin@126.com>
2017-03-26 14:01:04 +0800

    this's two file~.java and .class

---
+++ Learn_Git.java
@@ -0,0 +1,11 @@
+class Learn_Git {
+	public static void main(String[] args) {
+		int [] arr = new int[5];
+		for (int i=0;i<=arr.length-1 ;i++ ) {
+			arr[i] = i;
+		}
+		for (int a :arr ) {
+			System.out.println(a);
+		}
+	}
+}
